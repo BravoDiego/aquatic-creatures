52e5b87c34570217af95755d612aed67eb066d02
BRAVO Diego <diego.bravo.contact@gmail.com>
2025-10-14 22:12:29 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,7 @@
 
 #include "../lib/Point.hpp"
 #include "../lib/Line.hpp"
-
-
-void logVelocities(const sf::Vector2f& v1, const sf::Vector2f& v2) {
-    // Ouvre le fichier en mode append (ajout à la fin du fichier)
-    std::ofstream file("vitesses.txt", std::ios::app);
-
-    if (file.is_open()) {
-        // Écrit le temps et les magnitudes des vitesses dans le fichier
-        float m1 = magnitude(v1);
-        float m2 = magnitude(v2);
-        file << "Ec " << 0.5f * (m1 * m1 + m2 * m2) << "|";
-        file << "Vitesse P1: " << m1 << " | ";
-        file << "Vitesse P2: " << m2 << std::endl;
-
-        // Ferme le fichier
-        file.close();
-    } else {
-        std::cerr << "Erreur: Impossible d'ouvrir le fichier vitesses.txt" << std::endl;
-    }
-}
+#include "../lib/Muscle.hpp"
 
 int main() {
     // Création de la fenêtre
@@ -34,14 +15,14 @@ int main() {
     std::vector<Point> points;
     std::vector<Line> lines;
 
-    points.emplace_back(sf::Vector2f(150, 286), sf::Vector2f(150, -150));
+    points.emplace_back(sf::Vector2f(150, 286), sf::Vector2f(0, 0));
     points.emplace_back(sf::Vector2f(200, 200), sf::Vector2f(0, 0));
-    points.emplace_back(sf::Vector2f(250, 280), sf::Vector2f(-150, 150));
+    points.emplace_back(sf::Vector2f(250, 280), sf::Vector2f(0, 0));
     //points.emplace_back(sf::Vector2f(300, 200), sf::Vector2f(0, -180));
     //points.emplace_back(sf::Vector2f(100, 200), sf::Vector2f(-40, 180));
     lines.emplace_back(points[0], points[1]);
     lines.emplace_back(points[1], points[2]);
-    //lines.emplace_back(points[1], points[3]);
+    // lines.emplace_back(points[0], points[2]);
     //lines.emplace_back(points[3], points[4]);
     
 
@@ -49,7 +30,6 @@ int main() {
     sf::View view(sf::FloatRect({0.f, 0.f}, {800.f, 600.f}));
     const float cameraSpeed = 200.f;
     float zoomLevel = 1.f;
-
     // Boucle principale
     sf::Clock clock; // Pour calculer le temps écoulé
     while (window.isOpen()) {
@@ -100,8 +80,6 @@ int main() {
         for (auto& line : lines) {
             line.update();
         }
-        // logVelocities(Point1.get_Velocity(), Point2.get_Velocity());
-
         // Effacer la fenêtre
 
         window.clear(sf::Color::White);
